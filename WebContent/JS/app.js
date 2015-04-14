626e2ab481d6f54dc6048c37cd349e88ad64c213
Elizabeth <Elizabeth@192.168.1.32>
2015-04-14 09:28:00 -0500

    algo

--- WebContent/JS/app.js
+++ WebContent/JS/app.js
@@ -1,77 +1,53 @@
 function soloNumeros(e) {
-	// capturamos la tecla pulsada
 	var teclaPulsada = window.event ? window.event.keyCode : e.which;
 
-	// capturamos el contenido del input
-	//var valor = document.getElementById("inputNumero").value;
-
-	// 45 = tecla simbolo menos (-)
-	// Si el usuario pulsa la tecla menos, y no se ha pulsado anteriormente
-	// Modificamos el contenido del mismo añadiendo el simbolo menos al
-	// inicio
-	/*
-	 * if(teclaPulsada==45 && valor.indexOf("-")==-1) {
-	 * document.getElementById("inputNumero").value="-"+valor; }
-	 */
-
-	// 13 = tecla enter
-	// 46 = tecla punto (.)
-	// Si el usuario pulsa la tecla enter o el punto y no hay ningun otro
-	// punto
-	
-	 /* if(teclaPulsada==13 || (teclaPulsada==46 && valor.indexOf(".")==-1)) {
-	  return true; }*/
-	
-
-	// devolvemos true o false dependiendo de si es numerico o no
 	return /\d/.test(String.fromCharCode(teclaPulsada));
 }
 
-// movimiento
-
-var velocidad = 10, direccion = velocidad, iniciar = false, x = 22, y = 66;
-var  p = 0;
+var velocidad = 1, direccion = velocidad, x = 22, y = 70;
+var p = 0;
 var datos = [];
 window.addEventListener('load', init);
 function init() {
-	
+
 	var canvas = document.getElementById("micanvas");
+
 	var ctx = canvas.getContext("2d");
-	cinta(ctx,x,y);
-	
+
+	cinta(ctx, x, y);
+
 	document.getElementById('cargar').addEventListener('click', function() {
 		numero1 = document.getElementById('txt').value;
 		numero2 = document.getElementById('txt1').value;
-		
+
 		var nume = parseInt(numero1) + parseInt(numero2);
 		var aux = 0;
 		datos = [];
-		datos[0] = 0;
-		while (nume >= 0) {
-
-			
-			if (aux != parseInt(numero1)) {
-				datos[parseInt(aux) + 1] = 1;
-			} else {
-				datos[aux + 1] = 0;
-			}
-			aux = aux + 1;
-
-			nume--;
-		}
 		
+		datos.push(0);
+		
+		while (aux < parseInt(numero1)) {
+			datos.push(1);
+			aux ++;
+		}
+		datos.push(0);
+		aux = 0;
+		while(aux < parseInt(numero2) ){
+			datos.push(1);
+			aux++;
+		}
+
 		if (datos != null) {
 			x = 110;
 			canvas.width = canvas.width;
+
 			ctx.strokeRect(63, 40, 30, 35);
+
 			cinta(ctx, x, y);
+
 		}
-		
+
 		cambio();
-		
-		
-			
-		// valor=document.getElementById('txt1').value;
 
 	});
 
@@ -80,23 +56,26 @@ function init() {
 function draw(canvas, ctx, x, y) {
 	canvas.width = canvas.width;
 	cinta(ctx, x, y);
+
 	ctx.strokeRect(63, 40, 30, 35);
+
 }
 
 function moveAndDraw(canvas, ctx) {
-	// if(x>(canvas.width-20))direccion=-velocidad; // se mueve a la izquierdad
-	// if(x<(20))direccion=velocidad;//se meuve a la derecha
-	direccion = -velocidad;
-	x += direccion;
-	
-	setTimeout(function(){ draw(canvas, ctx, x, y)}, 400);
+
+	if (x > y) {
+		direccion = -velocidad;
+		x += direccion;
+		draw(canvas, ctx, x, y);
+	}
+
 }
 
 function cinta(ctx, x, y) {
 	p = 0;
 
 	for (var i = 0; i < datos.length; i++) {
-		ctx.font = "35px Arial";
+		ctx.font = "35px gothic";
 		ctx.strokeText(datos[i], x + p, y);
 		p = p + 30;
 	}
@@ -107,10 +86,11 @@ function cambio() {
 	var canvas = document.getElementById("micanvas");
 	var ctx = canvas.getContext("2d");
 	est = 0;
+	var c = x - 22;
 	automata = [ [ "0,0", "0,1" ], [ "1,2", "1,1" ] ];
 
 	/*
-	 *     0         1
+	 * 0 1
 	 * 
 	 * A "0,A,>" " 0,B,>"
 	 * 
@@ -119,34 +99,35 @@ function cambio() {
 
 	for (var i = 0; i < datos.length; i++) {
 		est = getState(est, datos[i]);
-		
+
 		if (datos[i] == 0 && est == 0) {
 
 			datos[i] = 0;
-			
 
 		} else if (datos[i] == 1 && est == 0) {
 
 			datos[i] = 0;
 			est = 1;
-			
+
 		}
 
 		else if (datos[i] == 1 && est == 1) {
 
 			datos[i] = 1;
-		
-			
+
 		} else if (datos[i] == 0 && est == 1) {
 
 			datos[i] = 1;
 			est = 2;
-			
-		}
-		moveAndDraw(canvas, ctx);
-	}
 
+		}
+		 setInterval(function() {
+				moveAndDraw(canvas, ctx);
+			
+		}, 1000);
+	}
 }
+
 
 function getState(state, leido) {
 	v = automata[state][leido].split(",");
